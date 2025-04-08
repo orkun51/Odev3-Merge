4d476e025abfc6a7f26bc5b5e2767ac055043d2a
orkunkocak7@gmail.com <orkunkocak7@gmail.com>
2025-04-08 23:10:30 +0300

    ogrenciler guncellendi

---
+++ automated_grading.py
@@ -0,0 +1,4 @@
+answer,score
+Photosynthesis converts sunlight into chemical energy in plants.,8
+Newton's law of gravity explains how two bodies attract each other.,7
+Water boils at 100¡C under standard atmospheric pressure.,9
